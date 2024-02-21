7502660aa8a86f1d4f0c08a502ec2c143c11ff26
Fleli <frederikedvardsen@gmail.com>
2024-02-21 14:29:42 +0100

    Added optional function in Navbar items

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,23 +1,25 @@
 import { Box, Button, ButtonGroup } from "@mui/material";
 import Link from "next/link";
 import React from "react";
+import { logOut } from "../utils/LocalStorage/profile";
 
 const Navbar = (props: {selected: number}) => {
   
   // Legg til flere meny-elementer her
   const menuItems = [
-    { text: "Flashy", link: "/homepage" },
-    { text: "Profil", link: "/profile" },
-    { text: "Logg ut", link: "/" },
+    { text: "Flashy", link: "/homepage", function: null },
+    { text: "Profil", link: "/profile", function: null },
+    { text: "Logg ut", link: "/", function: logOut },
   ]
   
   const reactElements = menuItems.map( item => {
     
+    const onClick = (item.function == null) ? ( () => {} ) : ( item.function );
     const variant = (menuItems.indexOf(item) == props.selected) ? "contained" : "outlined";
     
     return (
       <Link href={item.link}>
-        <Button variant={variant}>
+        <Button variant={variant} onClick={onClick}>
           {item.text}
         </Button>
       </Link>
