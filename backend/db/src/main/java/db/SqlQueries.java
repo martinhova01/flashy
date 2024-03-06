a1afb14e7092d88f0821810c87ddc0ef21becf4b
martin Høva <mrhova46@gmail.com>
2024-03-06 20:24:43 +0100

    added getOwner endpoint

--- backend/db/src/main/java/db/SqlQueries.java
+++ backend/db/src/main/java/db/SqlQueries.java
@@ -247,5 +247,11 @@ public class SqlQueries {
             Integer.toString(profileId));
     }
 
+    public static String getOwnerQuery(int deckId) {
+        return String.format("SELECT profile.firstname, profile.lastname "
+        + "FROM deck INNER JOIN profile ON (profile.profile_id = deck.owner_id) "
+        + "WHERE deck.owner_id = %s", Integer.toString(deckId));
+    }
+
     
 }
