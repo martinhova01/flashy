0e4aec8bdcb211df4d2cee45c3256fd298933b18
martin Høva <mrhova46@gmail.com>
2024-03-06 21:02:24 +0100

    removed unused file

--- frontend/src/app/utils/LocalStorage/deck.ts
+++
@@ -1,7 +0,0 @@
-import { requests } from "../Api/requests";
-import { DeckDto } from "../dto/DeckDto";
-
-// Gets all public decks from the database.
-export function getPublicDecks(): DeckDto[] {
-    return JSON.parse(localStorage.getItem("decks")!) as DeckDto[]
-}
